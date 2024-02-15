9d28c4f27a7920379a5fd374d4bdbc432652aaa2
zakisu250 <zakisu250@gmail.com>
2024-02-15 16:54:30 +0300

    Add app.py for session authentication

---
+++ 0x02-Session_authentication/api/v1/app.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+from os import getenv
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+if getenv("AUTH_TYPE") == "auth":
+    auth = Auth()
+if getenv("AUTH_TYPE") == "basic_auth":
+    auth = BasicAuth()
+if getenv("AUTH_TYPE") == "session_auth":
+    auth = SessionAuth()
+
+
+@app.before_request
+def handle_before_request():
+    """Filter requests"""
+    if auth is None:
+        return
+
+    excluded_paths = [
+        "/api/v1/status/",
+        "/api/v1/unauthorized/",
+        "/api/v1/forbidden/",
+        "/api/v1/auth_session/login/",
+    ]
+
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if (
+        auth.authorization_header(request) is None
+        and auth.session_cookie(request) is None
+    ):
+        abort(401)
+        return None
+
+    if auth.current_user(request) is None:
+        return abort(403)
+    request.current_user = auth.current_user(request)
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """Not found handler"""
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """Unauthorized error"""
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """Forbidden error"""
+    return jsonify({"error": "Forbidden"}), 403
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
